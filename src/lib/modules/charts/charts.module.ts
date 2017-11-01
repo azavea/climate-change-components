936ae59a9a9592322887e82ccf6b19f20386795e
CloudNiner <andrew@cloudniner.co>
2017-11-01 07:28:33 -0400

    Add ModelModalComponent

--- src/lib/modules/charts/charts.module.ts
+++ src/lib/modules/charts/charts.module.ts
@@ -1,20 +1,30 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import {
+  ModalModule
+} from 'ngx-bootstrap';
 
 import { DatasetToggleComponent } from './components/dataset-toggle/dataset-toggle.component';
 import { LineGraphComponent } from './components/line-graph/line-graph.component';
+import { ModelModalComponent } from './components/model-modal/model-modal.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ModalModule.forRoot()
   ],
   declarations: [
     DatasetToggleComponent,
-    LineGraphComponent
+    LineGraphComponent,
+    ModelModalComponent
   ],
   exports: [
     DatasetToggleComponent,
-    LineGraphComponent
+    LineGraphComponent,
+    ModelModalComponent
   ]
 })
 export class ChartsModule {}
