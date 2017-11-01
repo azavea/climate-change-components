936ee7e3b98a8f0cdd3a44ee7607852b03575e13
CloudNiner <andrew@cloudniner.co>
2017-11-01 10:40:38 -0400

    Add UnitsDropdownComponent

--- src/lib/modules/charts/charts.module.ts
+++ src/lib/modules/charts/charts.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import {
+  BsDropdownModule,
   ModalModule
 } from 'ngx-bootstrap';
 
@@ -10,24 +11,28 @@ import { DatasetToggleComponent } from './components/dataset-toggle/dataset-togg
 import { LineGraphComponent } from './components/line-graph/line-graph.component';
 import { ModelModalComponent } from './components/model-modal/model-modal.component';
 import { ScenarioToggleComponent } from './components/scenario-toggle/scenario-toggle.component';
+import { UnitsDropdownComponent } from './components/units-dropdown/units-dropdown.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    BsDropdownModule.forRoot(),
     ModalModule.forRoot()
   ],
   declarations: [
     DatasetToggleComponent,
     LineGraphComponent,
     ModelModalComponent,
-    ScenarioToggleComponent
+    ScenarioToggleComponent,
+    UnitsDropdownComponent
   ],
   exports: [
     DatasetToggleComponent,
     LineGraphComponent,
     ModelModalComponent,
-    ScenarioToggleComponent
+    ScenarioToggleComponent,
+    UnitsDropdownComponent
   ]
 })
 export class ChartsModule {}
