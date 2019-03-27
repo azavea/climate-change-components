c5ae1e95e515151fda9cc6693413a698110f9f3f
Derek Dohler <ddohler@azavea.com>
2019-03-27 11:33:31 -0400

    Incorporate chart data download services
    - Upgrades to latest version of save-svg-as-png to fix an issue with
    downloading PNGs
    - Adds a `selector` parameter to ImageExportService.downloadAsPNG() that
    can be used to dynamically remap CSS selectors; the previous value was
    hardcoded such that it worked only in the Lab.

---
+++ src/lib/modules/charts/services/image-export.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import * as SaveSvg from 'save-svg-as-png';
+
+/*
+ * Generates image of D3 chart SVG for download
+ */
+@Injectable()
+export class ImageExportService {
+
+    /**
+     * Options to pass when converting SVG to PNG
+     *
+     * @param parentSelector - Name of parent of chart, used to rewrite CSS selectors
+     */
+    private chartOptions(parentSelector: string) {
+        return {
+            backgroundColor: 'white',
+            selectorRemap: function(selector) {
+                // find CSS selectors mapped to parent chart
+                return selector.replace(parentSelector, '');
+            }
+        };
+    }
+
+    /**
+     * Converts chart SVG to PNG and downloads it.
+     *
+     * @param indicatorName - Name of indicator, used for SVG selector
+     * @param fileName - File name for download, will be suffixed with extension
+     */
+    public downloadAsPNG(indicatorName: string, fileName: string, selector: string): void {
+        const filename: string = fileName + '.png';
+        const svg: HTMLElement = document.getElementById('chart-' + indicatorName);
+        // SVG might not be found if chart hasn't loaded yet
+        if (!svg) { return; }
+
+        SaveSvg.saveSvgAsPng(svg, filename, this.chartOptions(selector));
+    }
+}
