bd72e27d80914d955ea51ee3057ab103bd1cc3c9
Michael Maurizi <mmaurizi@azavea.com>
2019-06-04 15:50:21 -0400

    move code into place

---
+++ climate-change-components/projects/climate-change-components/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of climate-change-components
+ */
+
+export * from './lib/modules/api';
+export * from './lib/modules/charts';
+export * from './lib/modules/shared';
--- src/lib/public_api.ts
+++
@@ -1,4 +0,0 @@
-
-export * from './modules/api';
-export * from './modules/charts';
-export * from './modules/shared';
